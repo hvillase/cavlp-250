d86fec9e230527f2216f1372c081953c4427159a
hvillase <hernani.vr@gmail.com>
2025-10-28 20:54:00 -0600

    clase5

---
+++ ejemplos/imagen/hydra_color.js
@@ -0,0 +1,28 @@
+
+ 
+// CANALES RGB
+//osc(2).color(3, 4, 6).out()
+
+// bins (enteros) y gamma (espacio de color)
+//osc().posterize(2, 0.4).out()
+
+// r g b a
+//osc(16).shift(0.1, 0.9, 0.1).out()
+
+// 0-1
+//osc(20).invert(0.2).out()
+
+//osc().contrast(20).out()
+//osc().brightness(0.5).out()
+
+// luminosidad de imagen, threshold, tolerance
+//osc(10, 0.01, 1).luma(0.1, 0.5).out()
+
+//osc(10, 0.01).thresh(0.4, 0.03).out()
+
+//osc(10, 0.01, 1).saturate(2).out()
+
+// tono
+//osc(10, 0.01, 1).hue(0.2).out()
+
+//osc(10, 0.01, 1).colorama(0.5).out()
